01ff3f57f7d0f316fa8f4a3a07ca30c06586312f
sonawaneteju4 <sonawaneteju4@gmail.com>
2024-01-12 17:00:07 +0530

    .

--- src/components/hooks/useCurrencyInfo.js
+++ src/components/hooks/useCurrencyInfo.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-  const [data, setdata] = useState("");
+  const [data, setdata] = useState({email:"",pass:""});
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
