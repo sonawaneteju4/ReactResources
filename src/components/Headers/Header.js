41b31e584750f75250e0fea6ad2a9053b7eaef9a
sonawaneteju4 <sonawaneteju4@gmail.com>
2024-01-15 12:37:29 +0530

    .

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -28,17 +28,7 @@ function Header() {
               <Link className="nav-link" to="about">
                 About
               </Link>
-              <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
+
               <Link className="nav-link" to="currencyCovertor">
               Currency Covertor
               </Link>
